25dbc8946a6998436eadb18d2c68da05a2dbc87d
BeagleSalad <145999204+BeagleSalad@users.noreply.github.com>
2024-10-30 09:36:30 +0700

    Delete aquarium_system directory

--- aquarium_system/aquarium/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Aquarium, Thermostat, TemperatureLog
-
-admin.site.register(Aquarium)
-admin.site.register(Thermostat)
-admin.site.register(TemperatureLog)
